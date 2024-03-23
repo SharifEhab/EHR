81a5ac1d317191adad71080e7221d7df688a1a80
Sharif Elmasry <Sharif.Mohammad02@eng-st.cu.edu.eg>
2024-03-23 08:54:18 +0000

    final

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -90,9 +90,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'lab3database',
-        'PASSWORD': 'Mypassword3',
-        'HOST': 'ehrserver.postgres.database.azure.com',
+        'USER': 'sharif',
+        'PASSWORD': 'Sharif2002',
+        'HOST': 'sharif.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS':{'sslmode':'require'},
     }
